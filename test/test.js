208e910bbffe19f60a690bbec0796103ded9cd33
Ruslan Ismagilov <is.ismagilov@gmail.com>
2014-06-11 23:58:25 +0400

    test: dummy cmd

--- test/test.js
+++ test/test.js
@@ -2,24 +2,25 @@ var shellby = require('../')
   , assert = require('assert')
   , exec = require('child_process').exec;
 
-describe('Test by file creation:', function() {
-  var captured_stdout = ''
-    , dummyFile = Math.random().toString(36).substring(7) + '.txt';
+var dummyFile = Math.random().toString(36).substring(7) + '.txt';
+
+describe('Create ' + dummyFile + ' with shellby:', function () {
+  var captured_stdout;
 
   before(function (done) {
-    exec('touch ' + dummyFile, function (error, stdout, stderr) {
-      if (error) done(error);
-      done();
-    });
+  	shellby.exec('touch ' + dummyFile, function (error) {
+  		if (error) done(error);
+  		done();
+  	});
   });
 
-  it('Should say that ' + dummyFile + ' file exist', function() {
+  it('Should say that file exist', function() {
     var cmd = '[ -f ' + dummyFile + ' ] && echo "exist"';
     exec(cmd, function (error, stdout, stderr) {
-      if (error) done(error);
       assert.equal(stdout, 'exist\n');
     });
   });
+
 
   after(function (done) {
   	exec('rm -rf ' + dummyFile, function (error, stdout, stderr) {
@@ -27,4 +28,12 @@ describe('Test by file creation:', function() {
       done();
     });
   });
+});
+
+describe('Dummy command', function () {
+  it('Should throw error', function () {    
+    shellby.exec('asasas', function (error) {
+    	assert.equal(/spawn ENOENT/.test(error), true);
+    });
+  });	
 });
